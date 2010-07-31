c003915edd9a8bc26c35978b52541c689a02be02
Fraser Speirs <fraser@speirs.org>
2010-07-31 15:10:20 +0100

    Added Google analytics

--- src/index.php
+++ src/index.php
@@ -478,6 +478,19 @@ $sections = array(
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="iasco.css" type="text/css" charset="utf-8">
 <title>International Apple Store Codes - IASCO</title>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-260012-8']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 
